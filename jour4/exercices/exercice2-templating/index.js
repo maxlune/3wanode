0a115601892c21cd33181e8bbea93e51d6963e5d
maxlune <85277579+maxlune@users.noreply.github.com>
2023-04-06 14:51:33 +0200

    jour 4 exercice 2

---
+++ jour4/exercices/exercice2-templating/index.js
@@ -0,0 +1,25 @@
+// 01. Écrire un script Node.js qui implémente renderFile et qui permet de rendre le template suivant.
+// L'objet user passé au template doit contenir la valeur suivante : { isAdmin: true }
+// if user.isAdmin
+//     h1 Access granted
+// else
+//     h1 You must be logged as an administrator!
+
+// (Vérifiez que le résultat du rendu change bien si vous passez isAdmin à false)
+
+// 02. Ré-écrire le même programme en utilisant cette fois la méthode compileFile.
+
+const pug = require("pug");
+
+// pug.renderFile(
+//   "./views/template.pug",
+//   { user: { isAdmin: true } },
+//   (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   }
+// );
+
+const compileTemplate = pug.compileFile("./views/template.pug");
+const result = compileTemplate({ user: { isAdmin: false } });
+console.log(result);
